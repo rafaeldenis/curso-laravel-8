8a4a7098b2a7bef7f91fb23e045a683d5c1a4976
rafaeldenis <rafagdf85@gmail.com>
2021-04-08 22:42:36 -0300

    atualizacao rota

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,33 @@ use Illuminate\Support\Facades\Route;
 use Symfony\Component\CssSelector\Node\FunctionNode;
 
 
+/*Route::group(['namespace' => 'Admin', 'prefix' => 'admin'], function() {
+   echo "kkkkkkkkkkkkkkk";
+});*/
+
+/*Route::middleware([])->group(function (){
+    Route::get('/posts',[PostController::class,'index'])->name('posts.index');
+
+}); */
+
+
+// Exemplo de Controlar grupos e middleware
+Route::group(['namespace' => 'Admin', 
+            'prefix' => 'admin',
+            'middleware' => 'auth'],
+             function() {
+                Route::any('/posts/search',[PostController::class,'search'])->name('posts.search');
+                Route::get('/posts',[PostController::class,'index'])->name('posts.index');
+                Route::get('/posts/create',[PostController::class,'create'])->name('posts.create');
+                Route::put('/posts/{id}',[PostController::class,'update'])->name('posts.update');
+                Route::get('/posts/edit/{id}',[PostController::class,'edit'])->name('posts.edit');
+                Route::delete('/posts/{id}',[PostController::class,'destroy'])->name('posts.destroy');
+                Route::get('/posts/{id}',[PostController::class,'show'])->name('posts.show');
+                
+                Route::post('/posts',[PostController::class,'store'])->name('posts.store');
+                
+});
+
 
 Route::get('/', function () {
     return view('welcome');
@@ -16,6 +43,11 @@ Route::get('/ola',function(){
     echo "OLÁ MUNDO DO LARAVEL 8";
 
 });
+
+Route::get('/login',function(){
+    return 'é necessário login para acessar os posts.... exemplo de middleware';
+
+})->name('login');
 
 Route::any('/posts/search',[PostController::class,'search'])->name('posts.search');
 Route::get('/posts',[PostController::class,'index'])->name('posts.index');
@@ -34,3 +66,5 @@ Route::put('/clientes/{id}',[ClienteController::class,'update'])->name('clientes
 Route::get('/clientes/edit/{id}',[ClienteController::class,'edit'])->name('clientes.edit');
 
 
+
+   
