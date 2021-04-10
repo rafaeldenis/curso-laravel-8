881140e78d34b979e8cc5f86a1f4fb53bbb74528
rafaeldenis <rafagdf85@gmail.com>
2021-04-10 18:15:07 -0300

    middleware controles

--- routes/web.php
+++ routes/web.php
@@ -16,26 +16,64 @@ use Symfony\Component\CssSelector\Node\FunctionNode;
 
 }); */
 
-
+/** Middleware de autorização das rotas */
+Route::group(['middleware' => ['auth']], function () {
 // Exemplo de Controlar grupos e middleware
 
-Route::group(['middleware' => ['auth','check.teste'],
-             'namespace' => 'Admin', 
-            'prefix' => 'admin'],
-             function() {
-                Route::any('/posts/search',[PostController::class,'search'])->name('posts.search');
-                Route::get('/posts',[PostController::class,'index'])->name('posts.index');
-                Route::get('/posts/create',[PostController::class,'create'])->name('posts.create');
-                Route::put('/posts/{id}',[PostController::class,'update'])->name('posts.update');
-                Route::get('/posts/edit/{id}',[PostController::class,'edit'])->name('posts.edit');
-                Route::delete('/posts/{id}',[PostController::class,'destroy'])->name('posts.destroy');
-                Route::get('/posts/{id}',[PostController::class,'show'])->name('posts.show');
+    Route::group(['middleware' => ['check.teste'],
+                //'namespace' => 'Admin', 
+                'prefix' => 'admin'],
+                function() {
+
                 
-                Route::post('/posts',[PostController::class,'store'])->name('posts.store');
+                    Route::any('/posts/search',[PostController::class,'search'])->name('posts.search');
+                    /*Route::get('/posts',[PostController::class,'index'])->name('posts.index');
+                    Route::get('/posts/create',[PostController::class,'create'])->name('posts.create');
+                    Route::put('/posts/{id}',[PostController::class,'update'])->name('posts.update');
+                    Route::get('/posts/edit/{id}',[PostController::class,'edit'])->name('posts.edit');
+                    Route::delete('/posts/{id}',[PostController::class,'destroy'])->name('posts.destroy');
+                    Route::get('/posts/{id}',[PostController::class,'show'])->name('posts.show');
+                    Route::post('/posts',[PostController::class,'store'])->name('posts.store');*/
+                    
+
+                /*Route::get('/clientes',[ClienteController::class,'index'])->name('clientes.index'); 
+                Route::get('/clientes/create',[ClienteController::class,'create'])->name('clientes.create'); 
+                Route::post('/clientes',[ClienteController::class,'store'])->name('clientes.store'); 
+                Route::put('/clientes/{id}',[ClienteController::class,'update'])->name('clientes.update');
+                Route::get('/clientes/edit/{id}',[ClienteController::class,'edit'])->name('clientes.edit');*/
+                Route::resource('/posts',PostController::class);
+                //Route::resource('/clientes',ClienteController::class);
+    });
+
+
+    Route::group(['middleware' => ['check.teste'],
+                //'namespace' => 'Admin', 
+                'prefix' => 'vendas'],
+                function() {
+
                 
+                    /*Route::get('/posts',[PostController::class,'index'])->name('posts.index');
+                    Route::get('/posts/create',[PostController::class,'create'])->name('posts.create');
+                    Route::put('/posts/{id}',[PostController::class,'update'])->name('posts.update');
+                    Route::get('/posts/edit/{id}',[PostController::class,'edit'])->name('posts.edit');
+                    Route::delete('/posts/{id}',[PostController::class,'destroy'])->name('posts.destroy');
+                    Route::get('/posts/{id}',[PostController::class,'show'])->name('posts.show');
+                    Route::post('/posts',[PostController::class,'store'])->name('posts.store');*/
+                    
+
+                /*Route::get('/clientes',[ClienteController::class,'index'])->name('clientes.index'); 
+                Route::get('/clientes/create',[ClienteController::class,'create'])->name('clientes.create'); 
+                Route::post('/clientes',[ClienteController::class,'store'])->name('clientes.store'); 
+                Route::put('/clientes/{id}',[ClienteController::class,'update'])->name('clientes.update');
+                Route::get('/clientes/edit/{id}',[ClienteController::class,'edit'])->name('clientes.edit');*/
+                //Route::resource('/posts',PostController::class);
+                Route::resource('/clientes',ClienteController::class);
+    });
+
+
 });
 
-
+//Route::resource('/clientes','App\Http\Controllers\ClienteController');
 Route::get('/', function () {
     return view('welcome');
 });
@@ -52,11 +90,6 @@ Route::get('/login',function(){
 
 
 
-Route::get('/clientes',[ClienteController::class,'index'])->name('clientes.index'); 
-Route::get('/clientes/create',[ClienteController::class,'create'])->name('clientes.create'); 
-Route::post('/clientes',[ClienteController::class,'store'])->name('clientes.store'); 
-Route::put('/clientes/{id}',[ClienteController::class,'update'])->name('clientes.update');
-Route::get('/clientes/edit/{id}',[ClienteController::class,'edit'])->name('clientes.edit');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
