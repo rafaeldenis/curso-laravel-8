640cbcb5ba7e6f64b05be13acb8db8021222c66a
rafaeldenis <rafagdf85@gmail.com>
2021-04-10 21:40:18 -0300

    ATUALIZACAO LAYOUT

--- resources/views/vendas/clientes/edit.blade.php
+++ resources/views/vendas/clientes/edit.blade.php
@@ -1,5 +1,8 @@
+@extends('admin.layouts.app')
+@section('title','EDITAR CLIENTE')
+@section('content')
 <h1>editar cliente</h1>
 <form action="{{ route('clientes.update', $cliente->id) }}" method="post">    
     @method('put')
-    @include('admin.clientes._partials.form')
+    @include('vendas.clientes._partials.form')
 </form>
